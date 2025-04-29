14737657aa184c5761bcee4f1c26818e207a10ff
pedro <phsilvamartins590@gmail.com>
2025-04-29 17:13:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.getElementById('frmCadastro').addEventListener('submit', async (e) => {
     e.preventDefault();
     const nome = document.getElementById('txtNome').value;
-    const email = document.getElementById('txtEmail').value;
+    const login = document.getElementById('txtLogin').value;
     const senha = document.getElementById('txtSenha').value;
     const tipo = 'cadastro';
 
     const response = await fetch('/api/mysql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ nome, email, senha, tipo })
+        body: JSON.stringify({ nome, login, senha, tipo })
     });
 
     const result = await response.json();
